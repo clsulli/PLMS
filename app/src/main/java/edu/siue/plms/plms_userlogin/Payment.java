882694be77f1168ddab6f64ea73fafbc1dc03aa1
clsulli <clsulli@siue.edu>
2018-05-30 07:09:47 -0500

    Added Payment widget

--- app/src/main/java/edu/siue/plms/plms_userlogin/Payment.java
+++ app/src/main/java/edu/siue/plms/plms_userlogin/Payment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +25,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,7 +36,9 @@ public class Payment extends AppCompatActivity {
     public static final String TAG = "LOGGING";
 
     //GUI Elements
-    private EditText inputEmail, inputPassword;
+    private TextView totalDue;
+    private Button c10, c25, c50, c100;
+    private double tempTotal;
 
     //Firebase Variables
     private DatabaseReference mFirebaseDatabase;
@@ -50,6 +55,13 @@ public class Payment extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
+        //Attach GUI elements to Variables
+        totalDue = (TextView) findViewById(R.id.totalDue);
+        c10 = (Button) findViewById(R.id.c10);
+        c25 = (Button) findViewById(R.id.c25);
+        c50 = (Button) findViewById(R.id.c50);
+        c100 = (Button) findViewById(R.id.c100);
+
         //Setup Firebase
         mFirebaseInstance = FirebaseDatabase.getInstance();
         mFirebaseDatabase = mFirebaseInstance.getReference();
@@ -59,7 +71,84 @@ public class Payment extends AppCompatActivity {
 
         //Get Logged In Application User and get unique User Id
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String userUid = user.getUid();
+        final String userUid = user.getUid();
+
+        DatabaseReference ref = mFirebaseDatabase.child("users").child(userUid);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User value = (User) dataSnapshot.getValue(User.class);
+                totalDue.setText(value.totalDue);
+                tempTotal = Double.parseDouble(value.totalDue.substring(1));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        c10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseDatabase = mFirebaseInstance.getReference();
+                DatabaseReference valRef = mFirebaseDatabase.child("users").child(userUid).child("totalDue");
+                double newValue;
+                newValue = tempTotal - 0.10;
+                if (newValue < 0.00) {
+                    newValue = 0.00;
+                }
+                String newValueStr = "$" + newValue;
+                valRef.setValue(newValueStr);
+            }
+        });
+
+        c25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseDatabase = mFirebaseInstance.getReference();
+                DatabaseReference valRef = mFirebaseDatabase.child("users").child(userUid).child("totalDue");
+                double newValue;
+                newValue = tempTotal - 0.25;
+                if (newValue < 0.00) {
+                    newValue = 0.00;
+                }
+                String newValueStr = "$" + newValue;
+                valRef.setValue(newValueStr);
+            }
+        });
+
+        c50.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseDatabase = mFirebaseInstance.getReference();
+                DatabaseReference valRef = mFirebaseDatabase.child("users").child(userUid).child("totalDue");
+                double newValue;
+                newValue = tempTotal - 0.50;
+                if (newValue < 0.00) {
+                    newValue = 0.00;
+                }
+                String newValueStr = "$" + newValue;
+                valRef.setValue(newValueStr);
+            }
+        });
+
+        c100.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseDatabase = mFirebaseInstance.getReference();
+                DatabaseReference valRef = mFirebaseDatabase.child("users").child(userUid).child("totalDue");
+                double newValue;
+                newValue = tempTotal - 1.00;
+                if (newValue < 0.00) {
+                    newValue = 0.00;
+                }
+                String newValueStr = "$" + newValue;
+                valRef.setValue(newValueStr);
+            }
+        });
+
 
         //Read database User value
 //        mFirebaseDatabase.child("users").child(userUid).addValueEventListener(new ValueEventListener() {
